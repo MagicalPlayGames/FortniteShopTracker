39fccc56d9559c49336e4ab97b323b68ac2a3e98
MagicalPlayGames <alcalamichael.mpgames@gmail.com>
2024-06-12 11:02:30 -0500

    Used PyLint for Cleanup

--- FortniteShopTracker/main.py
+++ FortniteShopTracker/main.py
@@ -1,22 +1,17 @@
+import os
+import requests
+
 from kivymd.app import MDApp
 from kivymd.uix.fitimage import FitImage
 from kivymd.uix.label import MDLabel
-from kivymd.uix.list import MDList
-from kivy.uix.image import Image, AsyncImage
 from kivymd.uix.gridlayout import MDGridLayout
-from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
-from kivymd.uix.swiper import MDSwiper, MDSwiperItem
-from kivy.clock import Clock
-from kivymd.uix.textfield import MDTextField
+from kivymd.uix.swiper import MDSwiperItem
 from kivy.uix.button import Button
-import requests
-
-import os
 from kivy.core.window import Window
 
-internet = False
-internetFailedKV = """
+Internet = False
+InternetFailedKV = """
 #:import MDLabel kivymd.uix.label.MDLabel
 MDBoxLayout:
     MDLabel:
@@ -35,19 +30,18 @@ class MyScroller(MDGridLayout):
 class MySwiper(MDSwiperItem):
     pass
 
-#When the image is clicked or tapped, the FitImage cycles through the images for that item
 class MyImage(FitImage):
+    """When the image is clicked or tapped, the FitImage cycles through the images for that item"""
     itemSelected = 0
     item = {}
     finalPrice = 0
     def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            if(len(self.item)<2):
-                return
+        if self.collide_point(*touch.pos) and len(self.item)>1:
             self.itemSelected+=1
             if(self.itemSelected>=len(self.item)):
                 self.itemSelected = 1
             self.source = self.item[self.itemSelected]
+
 class MyLabel(MDLabel):
     pass
 
@@ -112,20 +106,19 @@ class MyApp(MDApp):
 
     #Builds on App Start
     def build(self):
-        global internet
+        global Internet
         if(os.name=='nt'):
             self.statTextMultiplier = .1
             Window.size = (1080, 2115)
         else:
             print(Window.size)
-        if(internet):
-            usedItemNames = []
+        if(Internet):
             self.root = Builder.load_file('main.kv')
             self.generateAllTab()
             self.generateTabsAndBoxes()
             self.findStats('Wh34tl3y')
         else:
-            self.root = Builder.load_string(internetFailedKV)
+            self.root = Builder.load_string(InternetFailedKV)
         return self.root
 
     #Changes the ScrollView List Items depending on the category of the tab selected
@@ -135,7 +128,7 @@ class MyApp(MDApp):
         images = self.root.ids.scroller
         images.clear_widgets()
         if(text=="All"):
-            for layout, items in self.itemDict.items():
+            for items in self.itemDict.values():
                 self.generateBoxes(items)
         else:
             self.generateBoxes(self.itemDict[text])
@@ -177,29 +170,29 @@ class MyApp(MDApp):
 
         self.itemDict[layoutName][itemName]['finalPrice'] = item['finalPrice']
         self.itemDict[layoutName][itemName]['mainImage'] = self.bestFit(mainImages,{'Background':None,'featured':None})
-        
-        if('items' in item):
+
+        if 'items' in item:
             for itemImages in item['items']:
                 if 'images' in itemImages:
                     imageSet = itemImages['images']
-                    for key, image in imageSet.items():
+                    for key in imageSet.keys():
                         if key.lower().find('icon')==-1 and key.lower().find('feature')==-1:
-                            if imageSet[key]!=None:
+                            if imageSet[key] is not None:
                                 if 'large' in imageSet[key]:
                                     self.itemDict[layoutName][itemName][key] = imageSet[key]['large']
                                     self.checkLayout(imageSet[key]['large'],itemName,key)
                                 else:
                                     self.checkLayout(imageSet[key],itemName,key)
-                    if itemImages['variants']!=None:
+                    if itemImages['variants'] is not None:
                         for variants in itemImages['variants']:
-                            if variants['options']!=None:
+                            if variants['options'] is not None:
                                 for finalImg in variants['options']:
                                     self.checkLayout(finalImg['image'],itemName,finalImg['tag'])
 
     #adds img to any layout that has name in it using key
     def checkLayout(self,img,name,key):
         for keys, values in self.itemDict.items():
-            if(name in values):
+            if name in values:
                 if not key in values:
                     self.itemDict[keys][name][key] = img
 
@@ -210,12 +203,12 @@ class MyApp(MDApp):
         for item in items :
             itemName = ""
             layoutName = self.bestFit(item,{'brItems':{0:{'type':'displayValue'}},'layout':'name'})
-            if(layoutName==None):
+            if layoutName is None:
                 layoutName = "Other"
-            if('layoutId' in item):
-                if('Jam' in item['layoutId'] and item['tracks']!=None):
+            if 'layoutId' in item:
+                if 'Jam' in item['layoutId'] and item['tracks'] is not None:
                     for tracks in item['tracks']:
-                        if(len(tracks['title'])>30 or len(tracks['artist'])>35):
+                        if len(tracks['title'])>30 or len(tracks['artist'])>35:
                             temp = tracks['title'] + ' by ' + tracks['artist']
                             itemName += temp[0:25] + temp[25:30].replace(' ','\n',1) + temp[30:]+'\n'
                         else:
@@ -223,12 +216,12 @@ class MyApp(MDApp):
                     itemName+= str(item['finalPrice']) + ' Vbucks'
                     self.itemInit(layoutName,itemName,item)
                 else:
-                    if(item['bundle']!=None):
+                    if item['bundle'] is not None:
                         itemName = str(item['bundle']['name']) + '\n' + str(item['finalPrice']) + ' VBucks'
                     else:
                         itemName = shortenName(item['devName'],item['finalPrice'])
             else:
-                if(item['bundle']!=None):
+                if item['bundle'] is not None:
                     itemName = str(item['bundle']['name']) + '\n' + str(item['finalPrice']) + ' VBucks'
                 else:
                     itemName = shortenName(item['devName'],item['finalPrice'])
@@ -236,22 +229,22 @@ class MyApp(MDApp):
 
     #Gets both list of item entries using fortnite-api and getResponse()
     def findItems(self):
-        global internet
+        global Internet
         data = getResponse('http://fortnite-api.com/v2/shop')
-        if(data!=None):
-            internet = True
+        if data is not None:
+            Internet = True
             items = data['entries']
             self.splitItems(items)
         data = getResponse('http://fortnite-api.com/v2/shop/br')
-        if(data!=None):
-            internet = True
+        if data is not None:
+            Internet = True
             items = data['featured']['entries']
             self.splitItems(items)
         else:
-            internet=False
+            Internet=False
         temp_dict = self.itemDict
         self.itemDict = dict(sorted(temp_dict.items()))
-    
+
     #Gets the Player Stats for the gamertag name provided no matter the platform
     # raises error in textfield if the gamertag is not found on the site
     def findStats(self,name):
@@ -262,25 +255,29 @@ class MyApp(MDApp):
         param['name'] = name
         accountTypes = ['epic','psn','xbl']
         connected = False
-        for type in accountTypes:
-           param['accountType'] = type
+        for aType in accountTypes:
+           param['accountType'] = aType
            url = 'http://fortnite-api.com/v2/stats/br/v2'
            try:
             response = requests.get(url,params=param,headers=headers)
-            if(response.json()['status']==200):
+            if response.json()['status']==200:
                 self.setStats(response.json()['data'])
                 self.root.ids.gamerTag.text = name
                 connected = True
                 break
-           except requests.RequestException as e:
+           except requests.exceptions.Timeout:
+               print("Request timedout during findStats function")
                continue
-        global internet
-        internet = connected
+           except requests.RequestException as e:
+               print(e)
+               continue
+        global Internet
+        Internet = connected
         if not connected:
             self.root.ids.search.helper_text= "GamerTag Not Found"
             self.root.ids.search.error = True
             print("Could not find user")
-        
+
     #Sets the Stats View based on overall, solo, duos, and squads stats.
     # Keys can be changed to show other stats stored in the data
     # Trios are not displayed, because it is always equal to None in the fortnite-api
@@ -309,7 +306,7 @@ def shortenName(name,price):
     name = name.replace('MtxCurrency','VBucks')
     name = name[0:-15] + name[-15:].replace('for ','\n')
     name = name.replace('-1',str(price))
-    if('vtid' in name):
+    if 'vtid' in name:
         name = name[14:].replace('vtid_body_','')
         name = name.replace('1 x ','')
         return name
@@ -317,18 +314,19 @@ def shortenName(name,price):
     return name
 #Gets the request Response to the findItems() function
 def getResponse(url):
-    global internet
+    global Internet
     headers = {'Authorization' : 'a760eb7f-2f6b-4eab-80c0-a2053a58dc5e'}
     try:
         response = requests.get(url, headers=headers)
-        if(response.status_code!=200):
+        if response.status_code!=200:
             raise requests.RequestException("Error Connecting")
-        else:
-            internet=True
-            return response.json()['data']
+        Internet=True
+        return response.json()['data']
+    except requests.exceptions.Timeout:
+        print("Program has timed out while Authenticating")
     except requests.RequestException as e:
         print(e)
-        internet=False
+        Internet=False
 
 #Shortens the setStats() function by retrieving the stats for each stat type individually
 def getStatData(keys,data):
